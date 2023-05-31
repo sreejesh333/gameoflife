eb0153291f010cd5a1de36b130428ccca44a5ecc
sreejesh333 <sreejeshravindran523@gmail.com>
2023-05-31 08:25:49 +0530

    GREEN grid display both dead nd live passed

--- test_gameoflife.py
+++ test_gameoflife.py
@@ -1,4 +1,12 @@
 import gameoflife
 
-def test_gmeoflfe_display():
-    assert gameoflife.display_grid([[1,1,1,0,1],[1,1,1,0,1],[1,1,1,0,1],[1,1,1,0,1],[1,1,1,0,1]]) == '###.#\n###.#\n###.#\n###.#\n###.#\n'
+def test_gmeoflfe_display_dead():
+    assert gameoflife.display_grid([[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]]) == '#####\n#####\n#####\n#####\n#####'
+
+def test_gmeoflfe_display_dead_live():
+    assert gameoflife.display_grid([[1,1,1,1,1],[1,1,0,1,1],[1,1,0,1,1],[1,1,1,1,1],[1,1,1,0,1]]) == '#####\n##.##\n##.##\n#####\n###.#'
+
+
+
+# def test_gmeoflfe_play():
+#       assert gameoflife.ply_game([[1,1,1,1,1],[1,1,1,0,1],[1,1,1,0,1],[1,1,1,1,1],[1,1,1,1,1]]) == '#####\n#####\n#####\n#####\n#####\n'
