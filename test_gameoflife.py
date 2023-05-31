c1d0c52dfb0437ac1949140e08f725f62e3de476
sreejesh333 <sreejeshravindran523@gmail.com>
2023-05-31 07:10:02 +0530

    GREEN updated row col size to 5 5

--- test_gameoflife.py
+++ test_gameoflife.py
@@ -1,4 +1,4 @@
 import gameoflife
 
 def test_gmeoflfe_display():
-    assert gameoflife.display_grid([[1,1,1],[1,1,1],[1,1,1]]) == '###\n###\n###\n'
+    assert gameoflife.display_grid([[1,1,1,0,1],[1,1,1,0,1],[1,1,1,0,1],[1,1,1,0,1],[1,1,1,0,1]]) == '###.#\n###.#\n###.#\n###.#\n###.#\n'
