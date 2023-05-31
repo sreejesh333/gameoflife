ed874269f72831122746a8007af8edbbc9f212a0
sreejesh333 <sreejeshravindran523@gmail.com>
2023-05-31 07:11:04 +0530

    updated row col size

--- gameoflife.py
+++ gameoflife.py
@@ -3,9 +3,11 @@ def display_grid(grid) :
     
     for row in grid :
             for row1 in row:
-                 lst.append('#')
+                 if row1 == 1 :
+                      lst.append('#')
+                 else :
+                       lst.append('.')     
             lst.append("\n")
     a = "".join(lst)
     return a
 
-        
