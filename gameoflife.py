b48ef548765796fc49a2af3f6defe38c77d355e1
sreejesh333 <sreejeshravindran523@gmail.com>
2023-06-01 08:11:28 +0530

    updated function display_grid

--- gameoflife.py
+++ gameoflife.py
@@ -4,10 +4,15 @@ def display_grid(grid) :
     for row in grid :
             for row1 in row:
                  if row1 == 1 :
-                      lst.append('#')
-                 else :
-                       lst.append('.')     
-            lst.append("\n")
-    a = "".join(lst)
+                      lst.append('1')
+                 elif row1 == 0 :
+                      lst.append('0')
+
+            lst.append('\n')   
+    lst.pop()       
+    a = ''.join(lst)
     return a
 
+# def ply_game(grid) :
+#       a = display_grid(grid)
+#       for row in range(5) :
