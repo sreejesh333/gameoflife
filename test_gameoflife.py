ccd3f299f57930d0317908a5c8ee222b1352a8e0
sreejesh333 <sreejeshravindran523@gmail.com>
2023-06-02 06:29:28 +0530

    updated

--- test_gameoflife.py
+++ test_gameoflife.py
@@ -9,16 +9,10 @@ def test_gmeoflfe_display_dead_live():
 
 
 
-# def test_gmeoflfe_play():
-#       assert gameoflife.ply_game([[1,1,1,1,1],[1,1,1,0,1],[1,1,1,0,1],[1,1,1,1,1],[1,1,1,1,1]]) == '#####\n#####\n#####\n#####\n#####\n'
-
-def sample_board():
-    return [[0,1,0],[0,0,1],[1,1,1]]
-
 def test_next_live_or_dead():
+    sample_board = [[0,1,0],[0,0,1],[1,1,1]]
     expected_board = [[0,0,0],[1,0,1],[0,1,1]]
-    board = sample_board()
-    assert gameoflife.next_generation(board) == expected_board
+    assert gameoflife.next_generation(sample_board) == expected_board
 
 def test_nieghbours_live_dead():
     board = [[0,1,0],[0,1,0],[0,1,0]]
